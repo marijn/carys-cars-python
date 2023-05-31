a5ed67d9fef154a87bfbcb2a72b4b596ca45414f
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 10:59:58 +0200

    :construction: Identify relevant test.

---
+++ carys_cars/pricing_engine/test_duration_in_minutes.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestDurationInMinutes(unittest.TestCase):
+    def test_it_guards_against_negative_DurationInMinutes(self):
+        raise Exception("Not yet implemented")
+
+
+if __name__ == '__main__':
+    unittest.main()
