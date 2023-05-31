c344b1b535ed19df1ad13f2393c50980124b3ec2
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 11:12:52 +0200

    :alembic: Extract method.

--- carys_cars/pricing_engine/duration_in_minutes.py
+++ carys_cars/pricing_engine/duration_in_minutes.py
@@ -1,10 +1,12 @@
 class DurationInMinutes:
     def __init__(self, duration_in_minutes: int) -> None:
         if duration_in_minutes < 0:
-            raise SorryInvalidDurationInMinutes("Sorry, DurationInMinutes should be a positive value of at least 1")
+            raise SorryInvalidDurationInMinutes.because_it_should_be_positive()
 
         self.duration_in_minutes = duration_in_minutes
 
 
 class SorryInvalidDurationInMinutes(Exception):
-    pass
+    @classmethod
+    def because_it_should_be_positive(cls):
+        return SorryInvalidDurationInMinutes("Sorry, DurationInMinutes should be a positive value of at least 1")
