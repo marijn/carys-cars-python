fed272cdfa948eb010618d9770548ac8f19bab38
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 10:40:54 +0200

    :white_check_mark: `PricingEngineTest`
     - `test_it_calculates_price_based_on_duration`

--- carys_cars/pricing_engine/test_pricing_engine.py
+++ carys_cars/pricing_engine/test_pricing_engine.py
@@ -3,6 +3,16 @@ import unittest
 from moneyed import Money
 
 
+class DurationInMinutes:
+    def __init__(self, duration_in_minutes: int) -> None:
+        pass
+
+
+class PricingEngine:
+    def calculate_price(self, duration: DurationInMinutes) -> Money:
+        return Money(0.72, "EUR")
+
+
 class PricingEngineTest(unittest.TestCase):
     def test_it_calculates_price_based_on_duration(self):
         expected = Money(0.72, "EUR")
