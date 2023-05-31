87bec0a298e14919ad44efa2bc929590fe933219
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 11:03:32 +0200

    :mag: `TestDurationInMinutes`
     - `test_it_guards_against_negative_DurationInMinutes`

--- carys_cars/pricing_engine/test_duration_in_minutes.py
+++ carys_cars/pricing_engine/test_duration_in_minutes.py
@@ -1,8 +1,12 @@
 import unittest
 
+from carys_cars.pricing_engine.duration_in_minutes import DurationInMinutes
+
 
 class TestDurationInMinutes(unittest.TestCase):
     def test_it_guards_against_negative_DurationInMinutes(self):
+        with self.assertRaises(SorryInvalidDurationInMinutes, msg="Sorry, DurationInMinutes should be a positive value of at least 1"):
+            DurationInMinutes(-1)
         raise Exception("Not yet implemented")
 
 
