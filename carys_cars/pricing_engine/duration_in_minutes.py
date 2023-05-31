9b4b056aec4d7cc405828a0851e42dacd216c8e8
Marijn Huizendveld <marijn@huizendveld.com>
2023-05-31 11:07:32 +0200

    :mag: `TestDurationInMinutes`
     - `test_it_guards_against_negative_DurationInMinutes`

--- carys_cars/pricing_engine/duration_in_minutes.py
+++ carys_cars/pricing_engine/duration_in_minutes.py
@@ -1,3 +1,10 @@
 class DurationInMinutes:
     def __init__(self, duration_in_minutes: int) -> None:
+        if duration_in_minutes < 0:
+            raise SorryInvalidDurationInMinutes("Sorry, DurationInMinutes should be a positive value of at least 1")
+
         self.duration_in_minutes = duration_in_minutes
+
+
+class SorryInvalidDurationInMinutes(Exception):
+    pass
